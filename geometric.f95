84b471ed228bbc6028185dd59606b36ffaa32dfa
rodrigobijani <rodrigobijani@gmail.com>
2017-08-16 16:53:14 -0300

    add geometric module

---
+++ geometric.f95
@@ -0,0 +1,16 @@
+MODULE geometric
+! Module to generate geometric sources
+IMPLICIT NONE
+! Variables:
+INTEGER, PARAMETER:: SGL = SELECTED_REAL_KIND(p=8, r=8)
+INTEGER, PARAMETER:: DBL = SELECTED_REAL_KIND(p=4, r=4)
+INTEGER(KIND=DBL):: i, j, k
+
+
+CONTAINS
+
+
+
+
+
+END MODULE geometric
