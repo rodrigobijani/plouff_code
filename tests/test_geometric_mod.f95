8531474100550322c9b7592ab15d0e2c06bb80d3
rodrigobijani <rodrigobijani@gmail.com>
2017-12-22 19:11:41 -0200

    adding testing code for geometric module. Almost done.

--- tests/test_geometric_mod.f95
+++ tests/test_geometric_mod.f95
@@ -5,7 +5,7 @@ PROGRAM test_geometric_mod
  IMPLICIT NONE
  TYPE(polyprism_type):: pp ! the object
  INTEGER:: n , fid
- CHARACTER(LEN=15)::inputfile
+ CHARACTER(LEN=25)::inputfile
 
  ! 1) test #01 of geometric clear: see if all values are cleared by the subroutine:
  pp%z1    = 11.0
@@ -30,20 +30,20 @@ PROGRAM test_geometric_mod
  pp%nv = 5000
  n     = pp%nv
  CALL geometric_allocate(pp)
- IF(SIZE(pp%xv,1) /= n .OR. SIZE(pp%yp,1) /= n )THEN; PRINT*,'Test 01 of geometric_allocate FAILED' ; STOP ; ENDIF   
+ IF(SIZE(pp%xv,1) /= n .OR. SIZE(pp%yv,1) /= n )THEN; PRINT*,'Test 01 of geometric_allocate FAILED' ; STOP ; ENDIF   
  PRINT*,'(2) - Tests on geometric_allocate PASSED'
  PRINT *, ''  
 
  ! 3) test for input file:
- inputfile = 'test_dens.inp'
+ inputfile = '../tests/test_dens.inp'
  fid = 1
  CALL geometric_read_inputs(pp,fid,inputfile) 
  IF(pp%z1 == 0.0 .OR. pp%z1 == 0.0 )THEN; PRINT*,'Test 01 for z1 and z2 of geometric_read_inputs FAILED' ; STOP ; ENDIF
  IF(pp%nv == 0 )THEN; PRINT*,'Test 01 for nv of  geometric_read_inputs FAILED' ; STOP ; ENDIF
  IF(pp%props == 'none')THEN; PRINT*,'Test 01 for props name of geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%ppval == 0.0)THEN; PRINT*,'Test 01 for ppval of  geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%inc == 0.0)THEN; PRINT*,'Test 01 for inc of  geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%dec == 0.0)THEN; PRINT*,'Test 01 for dec of  geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%ppval == 0.0)THEN; PRINT*,'Test 01 for ppval of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%inc /= 0.0)THEN; PRINT*,'Test 01 for inc of  geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%dec /= 0.0)THEN; PRINT*,'Test 01 for dec of  geometric_read_inputs FAILED' ; STOP ; ENDIF
 
  PRINT*,'(3) - Tests on geometric_read_inputs PASSED'
  PRINT *, '' 
