e293f278076024013f20b2b46ea4f6168f52c2a8
rodrigobijani <rodrigobijani@gmail.com>
2018-01-03 15:08:57 -0200

    changes in test_geometric_mod.f95 - tests done.

--- tests/test_geometric_mod.f95
+++ tests/test_geometric_mod.f95
@@ -22,30 +22,55 @@ PROGRAM test_geometric_mod
  IF(pp%ppval /= 0.0)THEN; PRINT*,'Test 01 for ppval of geometric_clear FAILED' ; STOP ; ENDIF
  IF(pp%inc /= 0.0)THEN; PRINT*,'Test 01 for inc of geometric_clear FAILED' ; STOP ; ENDIF
  IF(pp%dec /= 0.0)THEN; PRINT*,'Test 01 for dec of geometric_clear FAILED' ; STOP ; ENDIF
-
+ IF(SIZE(pp%xv,1) /= 1 .OR. SIZE(pp%yv,1) /= 1 )THEN; PRINT*,'Test 01 for xv and yv of geometric_clear FAILED' ; STOP ; ENDIF 
  PRINT*,'(1) - Tests on geometric_clear PASSED'
  PRINT *, ''
+!-----------------------------------------------------------------------------------------------------------------------------------
 
- ! 2) test #02: check if geometric_allocate does really allocate arrays:
+! 2) test #02: check if geometric_allocate does really allocate arrays:
  pp%nv = 5000
  n     = pp%nv
  CALL geometric_allocate(pp)
- IF(SIZE(pp%xv,1) /= n .OR. SIZE(pp%yv,1) /= n )THEN; PRINT*,'Test 01 of geometric_allocate FAILED' ; STOP ; ENDIF   
+ IF(SIZE(pp%xv,1) /= n .OR. SIZE(pp%yv,1) /= n )THEN; PRINT*,'Test 02 of geometric_allocate FAILED' ; STOP ; ENDIF   
  PRINT*,'(2) - Tests on geometric_allocate PASSED'
  PRINT *, ''  
-
- ! 3) test for input file:
- inputfile = '../tests/test_dens.inp'
+!-----------------------------------------------------------------------------------------------------------------------------------
+ 
+! 4) test for input file:
+ inputfile = '../tests/test_dens.inp'  
+ 
+ ! cleanning the object:
+ CALL geometric_clear(pp)
+ ! read input file:
  fid = 1
  CALL geometric_read_inputs(pp,fid,inputfile) 
- IF(pp%z1 == 0.0 .OR. pp%z1 == 0.0 )THEN; PRINT*,'Test 01 for z1 and z2 of geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%nv == 0 )THEN; PRINT*,'Test 01 for nv of  geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%props == 'none')THEN; PRINT*,'Test 01 for props name of geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%ppval == 0.0)THEN; PRINT*,'Test 01 for ppval of geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%inc /= 0.0)THEN; PRINT*,'Test 01 for inc of  geometric_read_inputs FAILED' ; STOP ; ENDIF
- IF(pp%dec /= 0.0)THEN; PRINT*,'Test 01 for dec of  geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%z1 == 0.0 .OR. pp%z1 == 0.0 )THEN; PRINT*,'Test 04 for z1 and z2 of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%nv == 0 )THEN; PRINT*,'Test 04 for nv of  geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%props == 'none'.OR.pp%props=='susc')THEN; PRINT*,'Test 04 for props name of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%ppval == 0.0)THEN; PRINT*,'Test 04 for ppval of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%inc /= 0.0)THEN; PRINT*,'Test 04 for inc of  geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%dec /= 0.0)THEN; PRINT*,'Test 04 for dec of  geometric_read_inputs FAILED' ; STOP ; ENDIF
 
- PRINT*,'(3) - Tests on geometric_read_inputs PASSED'
- PRINT *, '' 
+ PRINT*,'(4) - Tests on geometric_read_inputs PASSED'
+ PRINT *, ''
+!-----------------------------------------------------------------------------------------------------------------------------------
+
+! 5) test for input file:
+ inputfile = '../tests/test_susc.inp'
+ ! cleanning the object:
+ CALL geometric_clear(pp)
+ ! read the input file:
+ fid = 2
+ CALL geometric_read_inputs(pp,fid,inputfile) 
+ IF(pp%z1 == 0.0 .OR. pp%z1 == 0.0 )THEN; PRINT*,'Test 05 for z1 and z2 of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%nv == 0 )THEN; PRINT*,'Test 05 for nv of  geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%props == 'none'.OR.pp%props=='dens')THEN; PRINT*,'Test 05 for props name of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%ppval == 0.0)THEN; PRINT*,'Test 05 for ppval of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%inc == 0.0)THEN; PRINT*,'Test 05 for inc of geometric_read_inputs FAILED' ; STOP ; ENDIF
+ IF(pp%dec == 0.0)THEN; PRINT*,'Test 05 for dec of geometric_read_inputs FAILED' ; STOP ; ENDIF
+
+ PRINT*,'(5) - Tests on geometric_read_inputs PASSED'
+ PRINT *, ''
+ PRINT*, '--------------- FINISHED -------------------------'
 
 END PROGRAM test_geometric_mod
